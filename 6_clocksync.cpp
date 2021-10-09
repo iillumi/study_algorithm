7cfd1e0b6729741c5ed6929c017b421e54dad78c
iillumi <wnstjr218@naver.com>
2021-10-09 22:23:26 +0900

    bruteforce / difficulty medium

--- 6_clocksync.cpp
+++ 6_clocksync.cpp
@@ -2,8 +2,8 @@
 #include <algorithm>
 using namespace std;
 
-int INF=987654321;
-char linkedclock[10][17] = {
+int INF=987654321; // 12시를 맞추기 위해 사용되는 큰 수
+char linkedclock[10][17] = { // 각각의 버튼에 연결된 시계('X')
 	"XXX.............",
 	"...X...X.X.X....",
 	"....X.....X...XX",
@@ -22,23 +22,23 @@ int main () {
 	int answer;
 	int c;
 	int arry[16]={0,};
-	cin >> c;
+	cin >> c; // testcase
 	for (int i=0;i<c;i++) {
 		for (int j=0;j<16;j++){
 			cin >> arry[j];
 		}
 		answer=findcase(arry,0);
-		cout << (answer==INF ? -1 : answer) << endl;
+		cout << (answer==INF ? -1 : answer) << endl; // 못 만들면 -1 출력
 	}
 	return 0;
 }
 
 int findcase(int *arry, int NumSwitch){
-	if(NumSwitch==10) return checkclock(arry) ? 0 : INF;
+	if(NumSwitch==10) return checkclock(arry) ? 0 : INF; // 10번 스위치까지 누르면 종료
 	int ret=INF;
-	for(int i=0;i<4;i++) {
+	for(int i=0;i<4;i++) { // 스위치를 4번 누르는 경우 수행 (4번 누르면 원래시간으로 복귀)
 		ret=min(ret,i+findcase(arry, NumSwitch+1));
-		for(int j=0;j<16;j++) {
+		for(int j=0;j<16;j++) { // 각각의 버튼에 연결된 시계의 시간 +3
 			if(linkedclock[NumSwitch][j]=='X'){
 				arry[j]+=3;
 				if(arry[j]==15) arry[j]=3;
@@ -48,7 +48,7 @@ int findcase(int *arry, int NumSwitch){
 	return ret;
 }
 
-bool checkclock (int * arry) {
+bool checkclock (int * arry) { // 시계가 모두 12시가 됐는지 확인
 	for(int i=0;i<16;i++) {
 		if(arry[i]!=12) return false;
 	}
